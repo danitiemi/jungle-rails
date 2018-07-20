7e60daac9349a43eddbc3375bb7e98876c771a98
danitiemi <danitiemi.br@gmail.com>
2018-07-20 06:15:41 +0000

    email receipt

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,13 @@
+class UserMailer < ApplicationMailer
+
+    default from: 'notifications@example.com'
+ 
+    def welcome_email
+      @user = params[:user]
+      @url  = 'http://example.com/login'
+      mail(to: @user.email, subject: 'Welcome to My Awesome Site') do |format|
+        format.html { render 'another_template' }
+        format.text { render plain: 'Render text' }
+        end
+    end
+end
